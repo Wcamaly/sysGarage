2d3447b4d66a8df46174d65ef614415c5c3ada9e
mrTater <waltercamaly@gmail.com>
2017-08-04 17:53:10 -0300

    test add user

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -8,6 +8,8 @@
 const app = require('../server/server.js');
 // Instance db
 const db = app.dataSources.mysqlDb;
+const User = app.models.User;
+const Role = app.models.Role;
 
 // List to Models 
 let models = ['User','AccessToken','ACL','RoleMapping','Role'];
@@ -16,6 +18,31 @@ let models = ['User','AccessToken','ACL','RoleMapping','Role'];
 db.automigrate(models, (err) => {
   if (err) throw new Error(err);
   console.log("Congratulations you created the tables successfully ");
+  //Create User Admin if not exist 
+  User.create([
+    {"username": "clientes", "email": "johns@doe.com", "password": "1234"},
+    {username: 'admin', email: 'jane@doe.com', password: 'admin'},
+    {username: 'Bob', email: 'bob@projects.com', password: 'opensesame'}
+  ], function(err, users) {
+    console.log(err)
+    if (err) throw err;
+
+    //create the admin role
+    Role.create({
+      name: 'admin'
+    }, function(err, role) {
+      if (err) cb(err);
+
+      //make bob an admin
+      role.principals.create({
+        principalType: RoleMapping.USER,
+        principalId: users[2].id
+      }, function(err, principal) {
+        cb(err);
+      });
+    });
+  });
+
   db.disconnect();
 })
 
